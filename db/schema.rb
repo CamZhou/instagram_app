8cc32ed954baa726f6db72a7e6299ac8480120ad
Haunter17 <haunterhacker17@gmail.com>
2017-02-06 22:39:44 -0800

    posting images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207050648) do
+ActiveRecord::Schema.define(version: 20170207061513) do
 
   create_table "models", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -31,8 +31,12 @@ ActiveRecord::Schema.define(version: 20170207050648) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
